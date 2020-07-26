98255d9b195650ad7993bb6bb419313ff4cea1ca
virgilt <travisjvirgil@gmail.com>
2020-07-26 18:40:46 -0400

    Fix preloader display.

--- src/components/previewCard.js
+++ src/components/previewCard.js
@@ -34,7 +34,18 @@ class PreviewCard extends Component {
                 )
             })
         ) : (
-            <div className="center">Loading fonts...</div>
+            // <div className="center">Loading fonts...</div>
+            <div className="preloader-wrapper active">
+                <div className="spinner-layer spinner-red-only">
+                <div className="circle-clipper left">
+                    <div className="circle"></div>
+                </div><div className="gap-patch">
+                    <div className="circle"></div>
+                </div><div className="circle-clipper right">
+                    <div className="circle"></div>
+                </div>
+                </div>
+            </div>
         )
         return (
             <div className={!this.props.grid ? "grid-container" : null}>
